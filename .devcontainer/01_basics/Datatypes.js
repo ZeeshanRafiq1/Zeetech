7827735e3e22011c52d491518f579c814347cf69
ZeeshanRafiq1 <zeeshanrafiq2380@gmail.com>
2024-07-29 20:37:19 +0000

    Datatypes

--- .devcontainer/01_basics/Datatypes.js
+++ .devcontainer/01_basics/Datatypes.js
@@ -3,13 +3,22 @@
 // alert(3 + 3) // we are using nodejs, not browser
 
 console.log(3 + 3) // code readability should be high
-let age = 18
-let isLoggedIn = false
-let state;
 
-// number = 2 to power 53
-// bigint
-// string = string ""
-// bolean = true/false
-// null = standalone value
-// undefined = 
+let name = "Zeeshan" // string datatype
+let age = 18 // number datatype
+let isLoggedIn = false/false // bolean datatype
+let state; // undefined datatype
+
+// number => 2 to power 53
+// bigint for large numbers
+// string => string ""
+// bolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => for determine of unique components
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null) // object
