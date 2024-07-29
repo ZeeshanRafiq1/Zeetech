d8272e1bea5db771c2e9f52492bd7999974c5575
ZeeshanRafiq1 <zeeshanrafiq2380@gmail.com>
2024-07-29 21:58:35 +0000

    Conversion of datatypes

---
+++ .devcontainer/01_basics/Conv.js
@@ -0,0 +1,47 @@
+let score = "Zeeshan"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// we found some conversions in investigation mention below
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+// "Zeeshan" => NaN
+
+
+let isLoggedIn = "Zeeshan"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// Conversion Investigation:
+// 1 => true; 0 => false
+// "" => false
+// "Zeeshan" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+
+console.log(typeof someNumber);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
