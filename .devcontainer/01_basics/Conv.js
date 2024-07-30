dc843be937c5f0ac2c518f63b8b3e41ca3d23de8
ZeeshanRafiq1 <zeeshanrafiq2380@gmail.com>
2024-07-30 20:23:39 +0000

    Operationes

--- .devcontainer/01_basics/Conv.js
+++ .devcontainer/01_basics/Conv.js
@@ -26,21 +26,51 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber);
+// console.log(stringNumber);
 
-console.log(typeof someNumber);
+// console.log(typeof someNumber);
 
+// *****************************Operations********************************
+// Example: 2+2=4 is an operation
+// If we bound two strings together that is also an operation
 
+// how we can get negative value, let's check
 
+let value = 3
+let negValue = -value
+// console.log(negValue);
 
+// console.log(2+2);
+// console.log(2-2);
+//console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(5%10);
 
+let str1 = "Hello"
+let str2 = " Zeeshan"
 
+let str3 = str1 + str2
+// console.log(str3);
 
+// console.log("1" + 2);
+// console.log(2 + "1");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
 
+/*
+Note if you use string value then your each value will show as string value
+if you use three value and first is string then other two will also show as string values with no conversion
+if you use third value as a string value then your first two values will be converted and third will remain string value
+*/
 
+// console.log(true);
+// console.log(+true);
+// console.log(+"");
 
-
-
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
 
 
 
