922576b7be5987764097eeeeb95314ae8d24673b
ZeeshanRafiq1 <zeeshanrafiq2380@gmail.com>
2024-08-01 22:12:57 +0000

    Strings methods

--- .devcontainer/01_basics/Datatypes_summary.js
+++ .devcontainer/01_basics/Datatypes_summary.js
@@ -1,4 +1,4 @@
-// primitve datatypes are 7: 
+// primitve datatypes are 7: Call by value 
 // string, number, boolean, null, undefined, symbols, bigInt
 
 // use of symbol
@@ -7,7 +7,7 @@ const anotherID = Symbol("123")
 // console.log(id === anotherID);
 const bigNumber = 2736548n
 
-// non-primitive (reference):
+// non-primitive (reference): Call by reference
 // array, objects, functions
 
 // array example: 
@@ -21,7 +21,37 @@ const myFunction = function(){
     console.log("Hello World");
 }
 
-console.log(typeof myFunction);
-console.log(typeof name);
+// console.log(typeof myFunction);
+// console.log(typeof name);
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Memory in javascript: there are two types of memory in JS
+
+// Stack memory, heap memory
+// Stack use in primitive datatypes
+// Heap use in non-primitive
+
+let myName = "ZeeshanRafiq"
+let anotherName = myName
+
+anotherName = "Zeeshan"
+
+console.log(myName);
+console.log(anotherName);
+
+
+let userOne = {
+    email: "Zeeshan@.com",
+    number: "030051"
+}
+
+let userTwo = userOne
+userTwo.email = "Zeeshan123.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+
 
 
---
+++ .devcontainer/01_basics/String.js
@@ -0,0 +1,56 @@
+const name = "Zeeshan"
+const repoCount = 50
+
+// console.log(name + repoCount + " Value");
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('ZeeshAn')
+
+// console.log(gameName[5]);
+// console.log(gameName.__proto__);
+// console.log(gameName.toUpperCase());
+
+// console.log(gameName.charAt(5));
+// console.log(gameName.indexOf('A'));
+
+const newString = gameName.substring(0, 5)
+// console.log(newString);
+
+const anotherString = gameName.slice(-7, 3)
+// console.log(anotherString);
+
+const newStringOne = "    Zee    "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+const url = "https://Zeeshan.com/Zeeshan20%Rafiq"
+// console.log(url.replace('https', 'start'));
+// console.log(url.includes('na'));
+
+const gameNameOne = new String('Zeeshan-A-com')
+console.log(gameNameOne.split('e'));
+
+console.log(gameName.bold());
+console.log(gameName.blink());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
